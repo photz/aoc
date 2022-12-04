396defa19424093f6a9def64d687c59b3f2de486
phtz <856205+photz@users.noreply.github.com>
2022-12-04 13:23:56 +0100

    Solve problem 4 in SQL

---
+++ day04.sql
@@ -0,0 +1,16 @@
+create or replace function parse_range(text) returns int8range language sql as $$
+  select int8range(split_part($1, '-', 1)::int,
+                   split_part($1, '-', 2)::int + 1)
+$$;
+
+create temp table raw_pairs (a text, b text);
+
+\copy raw_pairs from './day04.txt' with delimiter ',' csv
+
+create table pairs as select parse_range(a) as a, parse_range(b) as b from raw_pairs;
+
+select format('Part 1: %s', count(*)) from parsed where a @> b or b @> a;
+
+select format('Part 2: %s', count(*)) from parsed where a && b;
+
+
