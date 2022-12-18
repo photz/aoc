0de73e4760b8d36bedcdaddcae0d72b8d97ac248
phtz <856205+photz@users.noreply.github.com>
2022-12-18 14:07:44 +0100

    Format

--- day17.py
+++ day17.py
@@ -7,8 +7,8 @@ from itertools import cycle, islice
 from dataclasses import dataclass
 from bitstring import BitArray
 
-class Map(object):
 
+class Map(object):
     def __init__(self, width: int, preserve_rows: int):
         self.width = width
         self.preserve_rows = preserve_rows
@@ -27,7 +27,7 @@ class Map(object):
 
     def is_set(self, x: int, y: int) -> bool:
         idx = self._idx(x, y)
-        assert 0 <= idx 
+        assert 0 <= idx
         return self.array[idx]
 
     def drop_row(self):
@@ -37,9 +37,8 @@ class Map(object):
     def set_offset(self, new_offset: int):
         assert self.current_offset <= new_offset
 
-        self.array <<= (self.width * (new_offset - self.current_offset))
+        self.array <<= self.width * (new_offset - self.current_offset)
         self.current_offset = new_offset
-        
 
 
 def test_map_simple():
@@ -49,7 +48,7 @@ def test_map_simple():
     m.set(0, 1)
     assert m.is_set(0, 1)
     m.set(0, 2)
-    assert m.is_set(0, 2)    
+    assert m.is_set(0, 2)
 
     m.set(9, 0)
     assert m.is_set(9, 0)
@@ -58,6 +57,7 @@ def test_map_simple():
     assert m.is_set(0, 3)
     with pytest.raises(Exception):
         assert m.is_set(0, 0)
+
 
 def test_map_bumping():
     m = Map(5, 10)
@@ -78,6 +78,7 @@ def test_map_bumping():
     with pytest.raises(Exception):
         assert m.is_set(1, 13)
 
+
 @dataclass
 class Shape:
     width: int
@@ -93,39 +94,52 @@ class Shape:
             if v == 1:
                 yield x, y
 
+
 shapes = {
-    '-': Shape(4, 1, (1, 1, 1, 1)),
-    '+': Shape(
+    "-": Shape(4, 1, (1, 1, 1, 1)),
+    "+": Shape(
         3,
         3,
         (
-            0, 1, 0,
-            1, 1, 1,
-            0, 1, 0,
-        )
-    ),
-    'L': Shape(
-        3,
-        3,
-        (
-            0, 0, 1,
-            0, 0, 1,
-            1, 1, 1,
+            0,
+            1,
+            0,
+            1,
+            1,
+            1,
+            0,
+            1,
+            0,
         ),
     ),
-    '|': Shape(
+    "L": Shape(
+        3,
+        3,
+        (
+            0,
+            0,
+            1,
+            0,
+            0,
+            1,
+            1,
+            1,
+            1,
+        ),
+    ),
+    "|": Shape(
         1,
         4,
         (1, 1, 1, 1),
     ),
-    'cube': Shape(2, 2, (1, 1, 1, 1)),
+    "cube": Shape(2, 2, (1, 1, 1, 1)),
 }
 
 
 def falling_shapes() -> Generator[Shape, None, None]:
-    yield from cycle([
-        shapes['-'], shapes['+'], shapes['L'], shapes['|'], shapes['cube']
-    ])
+    yield from cycle(
+        [shapes["-"], shapes["+"], shapes["L"], shapes["|"], shapes["cube"]]
+    )
 
 
 def read_input(path: str) -> str:
@@ -150,7 +164,9 @@ def draw(cave_width, solidified, inflight: Tuple[int, int, Shape], draw_whole=Fa
         if inflight_x + shape.width < x:
             return False
 
-        return (x, y) in ((px + inflight_x, inflight_y - py) for px, py in shape.points())
+        return (x, y) in (
+            (px + inflight_x, inflight_y - py) for px, py in shape.points()
+        )
 
     def is_set(x, y):
         try:
@@ -160,27 +176,25 @@ def draw(cave_width, solidified, inflight: Tuple[int, int, Shape], draw_whole=Fa
 
     def char_at(x: int, y: int) -> str:
         if y == -1 and (x < 0 or cave_width <= x):
-            return '+'
+            return "+"
         elif y == -1:
-            return '-'
+            return "-"
         elif x < 0 or cave_width <= x:
-            return '|'
+            return "|"
         elif is_set(x, y):
-            return '#'
+            return "#"
         elif is_inflight(x, y):
-            return '@'
+            return "@"
         else:
-            return '.'
-        
+            return "."
+
     if draw_whole:
         until_y = -2
     else:
         until_y = max(inflight_y - 40, -2)
 
     for y in range(inflight_y + 3, until_y, -1):
-        print(''.join((char_at(x, y) for x in range(-1, cave_width + 1))))
-
-
+        print("".join((char_at(x, y) for x in range(-1, cave_width + 1))))
 
 
 def points(shape_x, shape_y, shape) -> Generator[Tuple[int, int], None, None]:
@@ -195,21 +209,27 @@ def collides(shape_x, shape_y, shape, solidified) -> bool:
 
     return False
 
+
 def on_ground(shape_y, shape) -> bool:
     return 0 == shape_y - shape.height + 1
 
+
 def can_move_left(shape_x, shape_y, shape, solidified):
-    return 0 < shape_x and \
-        not collides(shape_x - 1, shape_y, shape, solidified)
+    return 0 < shape_x and not collides(shape_x - 1, shape_y, shape, solidified)
+
 
 def can_move_right(shape_x, shape_y, shape, solidified):
     cave_width = 7
-    return shape_x < cave_width - shape.width \
-        and not collides(shape_x + 1, shape_y, shape, solidified)
+    return shape_x < cave_width - shape.width and not collides(
+        shape_x + 1, shape_y, shape, solidified
+    )
+
 
 def can_fall_further(shape_x, shape_y, shape, solidified):
-    return not on_ground(shape_y, shape) and \
-        not collides(shape_x, shape_y - 1, shape, solidified)
+    return not on_ground(shape_y, shape) and not collides(
+        shape_x, shape_y - 1, shape, solidified
+    )
+
 
 def simulate(cave_width: int, spawn_x: int, margin_y: int, shapes, gusts):
 
@@ -217,11 +237,11 @@ def simulate(cave_width: int, spawn_x: int, margin_y: int, shapes, gusts):
 
     current_h = -1
 
-    #rows = []
+    # rows = []
 
-    #states = {}
+    # states = {}
 
-    #f = open('pattern-example-with-fallen-shapes.txt', 'w')
+    # f = open('pattern-example-with-fallen-shapes.txt', 'w')
 
     for i, shape in enumerate(islice(falling_shapes(), 2022)):
 
@@ -231,20 +251,22 @@ def simulate(cave_width: int, spawn_x: int, margin_y: int, shapes, gusts):
         while True:
             direction = next(gusts)
 
-            #input('========= ' + direction + ' ========== ' + str(current_h))
-            #print(chr(27) + "[2J")
-            #draw(cave_width, solidified, (shape_x, shape_y, shape))
+            # input('========= ' + direction + ' ========== ' + str(current_h))
+            # print(chr(27) + "[2J")
+            # draw(cave_width, solidified, (shape_x, shape_y, shape))
 
-            if direction == '<' and can_move_left(shape_x, shape_y, shape, solidified):
+            if direction == "<" and can_move_left(shape_x, shape_y, shape, solidified):
                 shape_x -= 1
-            elif direction == '>' and can_move_right(shape_x, shape_y, shape, solidified):
+            elif direction == ">" and can_move_right(
+                shape_x, shape_y, shape, solidified
+            ):
                 shape_x += 1
 
             if not can_fall_further(shape_x, shape_y, shape, solidified):
                 break
 
             shape_y -= 1
-                
+
         # if solidified.array.b in states:
         #     print('seen this before', (i, current_h), states[solidified.array.b])
         #     prev_i, prev_current_h = states[solidified.array.b]
@@ -281,16 +303,8 @@ def simulate(cave_width: int, spawn_x: int, margin_y: int, shapes, gusts):
 
     return current_h + 1
 
-if __name__ == '__main__':
-    jetpattern = read_input('./day17.txt')
 
-    print(
-        simulate(
-            7,
-            2,
-            3,
-            falling_shapes(),
-            cycle(jetpattern)
-        )
-    )
-    
+if __name__ == "__main__":
+    jetpattern = read_input("./day17.txt")
+
+    print(simulate(7, 2, 3, falling_shapes(), cycle(jetpattern)))
